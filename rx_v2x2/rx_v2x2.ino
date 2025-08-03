6cc19fcb3f1d984789c93b9a71310564074e27ec
Fernando Pino <fapm10@gmail.com>
2025-08-03 14:17:12 -0400

    Implement forced hopping and extended
    Add extended packet decoding for throttle, trims, and ID

--- rx_v2x2/rx_v2x2.ino
+++ rx_v2x2/rx_v2x2.ino
@@ -22,23 +22,27 @@ const uint8_t freq_hopping[4][16] = {
   { 0x22, 0x27, 0x17, 0x39, 0x34, 0x28, 0x2B, 0x1D, 0x18, 0x2A, 0x21, 0x38, 0x10, 0x26, 0x20, 0x1F }
 };
 
-uint8_t txid[3] = { 177, 9, 156 }; // ID usado para seleccionar tabla hopping
+// ID usado para seleccionar tabla hopping
+// All - 177, 9, 156 
+// Tx WlToys A242 - 223, 150, 69
+// Tx iRangeX IRX6 - 158, 192, 5
+uint8_t txid[3] = { 223, 150, 69 }; 
 
-
-// Variables del receptor
-// Estado del protocolo V202 - Dirección RX (típica V202)
-uint8_t rx_address[5] = {0x66, 0x88, 0x68, 0x68, 0x68};
-uint8_t hopping_sequence[16];
-uint8_t packet[16];
-uint8_t bound_address[5] = {0};
-uint8_t rf_channels[16]; // tabla de canales 
+uint8_t rf_channels[16]; // tabla de canales calculada
 uint8_t rf_ch_num = 0;
+
+uint8_t packet[16];
+
 bool bound = false;
 unsigned long lastPacketTime = 0;
 
-// Rangos típicos del protocolo V202
-//#define MIN_CHANNEL 2
-//#define MAX_CHANNEL 80
+// Dirección RX (típica V202)
+// [102, 136, 104, 104, 104]
+uint8_t rx_address[5] = { 0x66, 0x88, 0x68, 0x68, 0x68 };
+
+// Hopping forzado por tiempo
+unsigned long lastHopTime = 0;
+const unsigned long hopInterval = 300; // ms
 
 void calcularHopping() {
   uint8_t sumId = txid[0] + txid[1] + txid[2];
@@ -51,53 +55,80 @@ void calcularHopping() {
   }
 }
 
+void configurarRadio() {
+  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CSN_PIN);
+
+  if (!radio.begin()) {
+    Serial.println(F(" !! No se detectó BK2425."));
+    while (1);
+  }
+
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(16);
+  radio.setCRCLength(RF24_CRC_16);
+
+  radio.openReadingPipe(0, rx_address);
+  radio.startListening();
+}
+
 bool validarChecksum(uint8_t* data) {
   uint8_t sum = 0;
   for (int i = 0; i < 15; i++) sum += data[i];
   return (sum == data[15]);
 }
 
-void configurarRadio() {
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("Iniciando BK2425 con ESP32...");
-
-  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CSN_PIN);
-
-  if (!radio.begin()) {
-    Serial.println("Fallo de comunicación.");
-    while (1);
-  }
-
-  radio.setPALevel(RF24_PA_LOW);
-  radio.setDataRate(RF24_1MBPS);  // Segun Protocolo 
-  radio.setCRCLength(RF24_CRC_16);
-  radio.setAutoAck(false);
-  //radio.setChannel(10);
-  //radio.setRetries(0, 0);
-  //radio.disableDynamicPayloads();    
-  radio.setPayloadSize(16);
-  radio.openReadingPipe(0, rx_address);
-  radio.startListening();
-
-  Serial.println("Buscando señal del control remoto...");
-  Serial.println("BK2425 iniciado correctamente.");
-  //radio.printDetails();
-}
-
-// Canales segun protocolo V2x2 
 void imprimirDatos(uint8_t* data) {
-  uint8_t throttle = data[0];
+  // Canales
+  uint8_t throttle = data[2];
   uint8_t yaw      = data[1];
-  uint8_t pitch    = data[2];
-  uint8_t roll     = data[4];
-  uint8_t flags    = data[5];
+  //uint8_t pitch      = data[0];
+  //uint8_t roll       = data[3];
+  // Trims - medio = 0x40 - 64 - 01000000
+  uint8_t yawTrim = data[4];
+  uint8_t thrTrim = data[5];
+  uint8_t rolTrim = data[6];
+
+  uint8_t C    = data[7];
+  uint8_t D    = data[8];
+  uint8_t E    = data[9];
+  uint8_t F   = data[10];
+  uint8_t G    = data[11];
+  uint8_t H    = data[12];
+  uint8_t I    = data[13];
+  uint8_t J    = data[14];
+  uint8_t K    = data[15];
+
 
   Serial.print(F("Throttle: ")); Serial.print(throttle);
   Serial.print(F(" | Yaw: ")); Serial.print(yaw);
-  Serial.print(F(" | Pitch: ")); Serial.print(pitch);
-  Serial.print(F(" | Roll: ")); Serial.print(roll);
-  Serial.print(F(" | Flags: 0b")); Serial.println(flags, BIN);
+  //Serial.print(F(" | Pitch: ")); Serial.print(pitch);
+  //Serial.print(F(" | Roll: ")); Serial.print(roll);
+  Serial.print(F(" | T.thr: ")); Serial.print(thrTrim);
+  Serial.print(F(" | T.yaw: ")); Serial.print(yawTrim);
+  //Serial.print(F(" | T.rol: ")); Serial.print(rolTrim);
+  Serial.print(F(" | Flags: 0b")); Serial.println(F, BIN); 
+ 
+
+  Serial.print(C);
+  Serial.print(" | ");
+  Serial.print(D);
+  Serial.print(" | ");
+  Serial.print(E);
+  Serial.print(" TxID|  ");
+  Serial.print(F);
+  Serial.print("|");
+  Serial.print(G);
+  Serial.print("|");
+  Serial.print(H);
+  Serial.print("|");
+  Serial.print(I);
+  Serial.print("|");
+  Serial.print(J);
+  Serial.print("|");
+  Serial.print(K); 
+  Serial.println("|");
 }
 
 void setup() {
@@ -112,51 +143,7 @@ void setup() {
   lastPacketTime = millis();
 }
 
-void bind_loop() {
-  for (uint8_t ch = MIN_CHANNEL; ch <= MAX_CHANNEL; ch++) {
-    radio.setChannel(ch);
-    radio.startListening();
-    Serial.printf("Canal %d...\n", ch);
-
-    for (int i = 0; i < BIND_RETRIES; i++) {
-      if (radio.available()) {
-        radio.read(&packet, sizeof(packet));
-
-        Serial.print("Paquete recibido: ");
-        for (int j = 0; j < 16; j++) {
-          Serial.printf("%02X ", packet[j]);
-        }
-        Serial.println();
-
-        // Verificar si el paquete parece válido 
-        if ((packet[0] ^ packet[1]) == 0xFF && packet[4] == 0xA7) {
-          memcpy(bound_address, packet + 2, 5);
-          current_channel = ch;
-          bound = true;
-          Serial.printf("✓ ¡Bind exitoso! Canal %d | Dirección: %02X %02X %02X %02X %02X\n",
-            current_channel,
-            bound_address[0], bound_address[1],
-            bound_address[2], bound_address[3],
-            bound_address[4]
-          );
-          return;
-        }
-      }
-      delay(5);
-    }
-    radio.stopListening();
-  }
-}
-
-void start_reception() {
-  radio.stopListening();
-  radio.openReadingPipe(0, bound_address);
-  radio.setChannel(current_channel);
-  radio.startListening();
-  Serial.println("Receptor conectado, escuchando paquetes...");
-}
-
-void loop() {  
+void loop() {
   if (radio.available()) {
     radio.read(packet, sizeof(packet));
     lastPacketTime = millis();
@@ -164,23 +151,37 @@ void loop() {
     if (validarChecksum(packet)) {
       if (!bound) {
         bound = true;
-        Serial.println(F("✓ Bind detectado. Escuchando control..."));
+        Serial.println(F("Bind detectado. Escuchando control..."));
       }
       imprimirDatos(packet);
 
       // Hopping a siguiente canal
-      rf_ch_num = (rf_ch_num + 1) % 16;
-      radio.setChannel(rf_channels[rf_ch_num]);
+      //rf_ch_num = (rf_ch_num + 1) % 16;
+      //radio.setChannel(rf_channels[rf_ch_num]);
+      //Serial.print(F("Hopping al canal: "));
+      //Serial.println(rf_channels[rf_ch_num], HEX);
+      //delay(2);
     } else {
       Serial.println(F("Paquete con checksum inválido"));
     }
-  } else {
-    // Timeout: si pasaron > 2 segundos sin paquete, se pierde bind
-    if (bound && millis() - lastPacketTime > 2000) {
-      Serial.println(F("Perdida de señal, buscando bind..."));
-      bound = false;
-      rf_ch_num = 0;
-      radio.setChannel(rf_channels[rf_ch_num]);
-    }
+  } 
+ // Forzar salto de canal por tiempo
+  if (millis() - lastHopTime > hopInterval) {
+    rf_ch_num = (rf_ch_num + 1) % 16;
+    radio.setChannel(rf_channels[rf_ch_num]);
+    Serial.print(F("Hopping al canal: "));
+    Serial.println(rf_channels[rf_ch_num], HEX);
+    lastHopTime = millis();
+  }
+
+  // Timeout: si pasan 4 segundos sin paquete, se pierde bind
+  if (bound && millis() - lastPacketTime > 4000) {
+    Serial.println(F("Perdida de señal, buscando bind..."));
+    bound = false;
+    rf_ch_num = 0;
+    radio.setChannel(rf_channels[rf_ch_num]);
+    lastHopTime = millis();
+    Serial.print(F("Canal reiniciado: "));
+    Serial.println(rf_channels[rf_ch_num], HEX);
   }
 }
